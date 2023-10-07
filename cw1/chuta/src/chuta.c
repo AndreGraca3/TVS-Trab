d047a437ab78658c70ded285995f3d47ef5f44c0
48459 - Diogo Santos <a48459@alunos.isel.pt>
2023-10-07 14:05:19 +0100

    first implementation of CW1-3

--- cw1/chuta/src/chuta.c
+++ cw1/chuta/src/chuta.c
@@ -12,8 +12,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
 
 static int run_test(test_function tfunc);
+
+static int run_prog(prog_test_spec prog);
 
 void run_function_tests(test_function tests[], size_t num_tests, bool stop_at_first_failure) {
 	int executed = 0;
@@ -74,4 +78,73 @@ static int run_test(test_function tfunc) {
 		waitpid(pid, &res, 0);
 		return res;
 	}
+}
+
+void run_stdio_program_tests(prog_test_spec tests[], size_t num_tests, bool stop_at_first_failure) {
+	for(int i = 0; i < num_tests; i++) {
+		prog_test_spec prog = tests[i];
+		printf("[%s]:", prog.test_name);
+		int res = run_prog(prog);
+		if (res != 0) {
+			puts("Assertion Failed!");
+			if (stop_at_first_failure) {
+				return;
+			}
+		} else {
+			puts("Assertion Succeded!");
+		}
+	}
+}
+
+static int run_prog(prog_test_spec prog) {
+	int pipefd[2];
+	pipe(pipefd);
+
+	pid_t pid = fork();
+	if (pid == 0) {
+		// child 
+		close(pipefd[0]);
+
+		dup2(pipefd[1],1); // redirec child's output to pipe.
+		close(pipefd[1]);
+
+		int inputFile = open(prog.input_path, O_RDONLY, 0640);
+		if (inputFile == -1) {
+			perror("open out.txt failed");
+			exit(1);
+		}
+		dup2(inputFile, 0); // redirec child's input to in.txt
+		close(inputFile); // close file
+
+		execlp(prog.command, prog.command, NULL);
+	} else {
+		// parent
+		close(pipefd[1]);
+
+		int outputFile = open(prog.output_path, O_RDONLY, 0640);
+		if (outputFile == -1) {
+			perror("open out.txt failed");
+			exit(1);
+		}
+	
+		char expected[256];
+		char actual[256];
+		for (;;) {
+			int nExpected = read(outputFile, expected, 255);
+			int nActual = read(pipefd[0], actual, 255);
+			if (nExpected == 0 || nActual == 0) break;
+			
+			expected[nExpected] = 0;
+			actual[nActual] = 0;	
+		}
+		close(pipefd[0]);
+		/*printf("[Expected]: %s", expected);
+		printf("--------------------------------\n");
+		printf("[Actual]: %s", actual);*/
+
+		if (strcmp(actual, expected) != 0) {
+			return -1;
+		}
+	}
+	return 0;
 }
--- cw1/demo/test/src/run_tests.c
+++ cw1/demo/test/src/run_tests.c
@@ -25,27 +25,28 @@ test_function all_function_tests[] = {
 // 
 // DECLARE ALL PROGRAM TESTS
 //
-//prog_function all_program_tests[] = {
-//	/* ... */
-//};
+prog_test_spec all_program_tests[] = {
+	{"cat test", "cat", "./input.txt", "./cat_output.txt"}, // Succeded!
+	{"grep test", "grep -i ficheiro", "./input.txt", "./grep_output.txt"},
+	{"sort test", "sort", "./input.txt", "./sort_output.txt"}, // Succeded!
+	{"another sort test", "sort", "./input.txt", "./wc_output.txt"}, // Note: this test is meant to fail !
+	{"wc test", "wc", "./input.txt", "./wc_output.txt"} // Succeded!
+};
 
 ////////////////////////
 // 
 // RUN ALL TESTS
 //
-// (do not modify, except to add the call to run_stdio_program_tests)
-//
-
 #define ARRLEN(arr) (sizeof(arr)/sizeof(arr[0]))
 
 const size_t num_function_tests = ARRLEN(all_function_tests);
-//const size_t num_program_tests = ARRLEN(all_program_tests);
+const size_t num_program_tests = ARRLEN(all_program_tests);
 
 int main() {
 	
-	run_function_tests(all_function_tests, num_function_tests, false);
+	//run_function_tests(all_function_tests, num_function_tests, false);
 	
-	//run_stdio_program_tests(...);
+	run_stdio_program_tests(all_program_tests, num_program_tests, false);
 	
 	return 0;
 }
